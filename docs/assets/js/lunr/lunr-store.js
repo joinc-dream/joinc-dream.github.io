a858dbac4e6c31c64427450f2f9eb669de1b0799
윤상배 <sbyun@cloit.com>
2026-02-21 18:36:10 +0900

    Deploy latest build to docs/

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -12,56 +12,56 @@ var store = [{
         "teaser": null
       },{
         "title": "GPT-OSS 사용기",
-        "excerpt":"새로운 오픈소스 LLM의 등장 OpenAI가 2019년 GPT-2를 시장에 내놓은 이후 6년만에 드디어 오픈소스 모델인 GPT-OSS(gpt-oss-20b, gpt-oss-120b)를 공개했습니다. OpenAI가 그동안 내놓았던 GPT-3, GPT-4는 모두 독점모델(Closed-source)이었기 때문에 왜 갑자기 오픈소스 모델을 공개했는지에 대한 여러 분석이 있었습니다. 치열해진 AI 시장에서의 주도권 경쟁 역시 가장 큰 이유는 메타의 Llama, 구글 Gemma, 중국의 DeepSeek/Qwen과 같은...","categories": ["LLM","생성형-AI","GPT-OSS","ollama","AI-트렌드"],
+        "excerpt":"새로운 오픈소스 LLM의 등장 OpenAI가 2019년 GPT-2를 시장에 내놓은 이후 6년만에 드디어 오픈소스 모델인 GPT-OSS(gpt-oss-20b, gpt-oss-120b)를 공개했습니다. OpenAI가 그동안 내놓았던 GPT-3, GPT-4는 모두 독점모델(Closed-source)이었기 때문에 왜 갑자기 오픈소스 모델을 공개했는지에 대한 여러 분석이 있었습니다. 치열해진 AI 시장에서의 주도권 경쟁 역시 가장 큰 이유는 메타의 Llama, 구글 Gemma, 중국의 DeepSeek/Qwen과 같은...","categories": ["Generative AI","Practical Lab"],
         "tags": [],
-        "url": "/llm/%EC%83%9D%EC%84%B1%ED%98%95-ai/gpt-oss/ollama/ai-%ED%8A%B8%EB%A0%8C%EB%93%9C/chat-gpt-oss/",
+        "url": "/generative%20ai/practical%20lab/chat-gpt-oss/",
         "teaser": null
       },{
         "title": "Vibe 코딩 한달살이",
-        "excerpt":"AI 코딩 어시스턴트 AI 어이스턴트를 이용한 개발은 대략 2021년 이후 일상화된 것으로 알려져 있다. OpenAI Codex, GitHub Copilot 코딩 AI 어시스턴트의 시작을 알렸고, 그 이후 Cursor, Windsurf, Replit AI, Claude Code, Gemini CLI 등 여러 도구들이 실무에서 사용되기 시작했다. 이 때가 2021년이니 코딩에서 AI의 도움을 받기 시작한 건 꽤 오래됐다고...","categories": ["LLM","생성형-AI","AI-코딩","Vibe-Coding","소프트웨어-엔지니어링"],
+        "excerpt":"AI 코딩 어시스턴트 AI 어이스턴트를 이용한 개발은 대략 2021년 이후 일상화된 것으로 알려져 있다. OpenAI Codex, GitHub Copilot 코딩 AI 어시스턴트의 시작을 알렸고, 그 이후 Cursor, Windsurf, Replit AI, Claude Code, Gemini CLI 등 여러 도구들이 실무에서 사용되기 시작했다. 이 때가 2021년이니 코딩에서 AI의 도움을 받기 시작한 건 꽤 오래됐다고...","categories": ["AI Engineering","Tech Strategy"],
         "tags": [],
-        "url": "/llm/%EC%83%9D%EC%84%B1%ED%98%95-ai/ai-%EC%BD%94%EB%94%A9/vibe-coding/%EC%86%8C%ED%94%84%ED%8A%B8%EC%9B%A8%EC%96%B4-%EC%97%94%EC%A7%80%EB%8B%88%EC%96%B4%EB%A7%81/vibe-coding/",
+        "url": "/ai%20engineering/tech%20strategy/vibe-coding/",
         "teaser": null
       },{
         "title": "8월 3째주 AI 뉴스 및 트렌드",
-        "excerpt":"GPT-5 정식 출시 OpenAI ChatGPT-5 launch live updates — all the latest news and biggest upgrades Everything you need to know about the new GPT-5 models www.tomsguide.com GPT-5는 야심차게 출시했으나, 일부 사용자들은 이전 모델인 GPT-4o의 감성적이고 창의적인 특성을 그리워하며 불만을 제기했습니다. 이에 대해 OpenAI는 GPT-4o를 ChatGPT Plus 사용자에게 재도입할 계획을...","categories": ["LLM","생성형-AI","AI-Agent","Vertical-AI","AI-트렌드"],
+        "excerpt":"GPT-5 정식 출시 OpenAI ChatGPT-5 launch live updates — all the latest news and biggest upgrades Everything you need to know about the new GPT-5 models www.tomsguide.com GPT-5는 야심차게 출시했으나, 일부 사용자들은 이전 모델인 GPT-4o의 감성적이고 창의적인 특성을 그리워하며 불만을 제기했습니다. 이에 대해 OpenAI는 GPT-4o를 ChatGPT Plus 사용자에게 재도입할 계획을...","categories": ["AI Intelligence"],
         "tags": [],
-        "url": "/llm/%EC%83%9D%EC%84%B1%ED%98%95-ai/ai-agent/vertical-ai/ai-%ED%8A%B8%EB%A0%8C%EB%93%9C/ai-trends/",
+        "url": "/ai%20intelligence/ai-trends/",
         "teaser": null
       },{
         "title": "Gemini 사용에 대한 코드 정리(VertexAI vs GenAI)",
-        "excerpt":"라이브러리 Import 공통적으로 사용하는 라이브러리를 불러옵니다. import os from dotenv import load_dotenv 기존 VertexAI SDK 사용 google.oauth2의 service_account를 활용 GCP의 Credential 파일을 통해 별도 로그인 없이 VertexAI 사용 from google.oauth2 import service_account import vertexai from vertexai.generative_models import ( GenerativeModel, GenerationConfig, Tool, grounding, ) VERTEXAI_PROJECT_ID: Vertex AI 사용 권한이 있는 GCP...","categories": ["LLM","생성형-AI","Quick","Lab"],
+        "excerpt":"라이브러리 Import 공통적으로 사용하는 라이브러리를 불러옵니다. import os from dotenv import load_dotenv 기존 VertexAI SDK 사용 google.oauth2의 service_account를 활용 GCP의 Credential 파일을 통해 별도 로그인 없이 VertexAI 사용 from google.oauth2 import service_account import vertexai from vertexai.generative_models import ( GenerativeModel, GenerationConfig, Tool, grounding, ) VERTEXAI_PROJECT_ID: Vertex AI 사용 권한이 있는 GCP...","categories": ["Practical Lab"],
         "tags": [],
-        "url": "/llm/%EC%83%9D%EC%84%B1%ED%98%95-ai/quick/lab/gemini-use/",
+        "url": "/practical%20lab/gemini-use/",
         "teaser": null
       },{
         "title": "Local LLM 호스팅을 위한 ollma 가이드",
-        "excerpt":"Ollama ? Ollama는 LLM(거대 언어 모델)을 쉽고 편리하게 실행하고 관리 할 수 있도록 도와주는 추론엔진을 포함한 종합적인 플랫폼입니다. LLM의 전체 생명주기는 학습(Training) 과 추론(Interence)로 나누어 볼 수 있습니다. 메타나 구글, OpenAI 등에서 학습한 LLM을 배포하면, 추론 엔진을 이용해서 사용하는 방식입니다. 즉 추론이란 이미 만들어서 배포한 모델을 활용해 사용자의 질문에 답하거나...","categories": ["LLM","ollama","sLLM","생성형-AI"],
+        "excerpt":"Ollama ? Ollama는 LLM(거대 언어 모델)을 쉽고 편리하게 실행하고 관리 할 수 있도록 도와주는 추론엔진을 포함한 종합적인 플랫폼입니다. LLM의 전체 생명주기는 학습(Training) 과 추론(Interence)로 나누어 볼 수 있습니다. 메타나 구글, OpenAI 등에서 학습한 LLM을 배포하면, 추론 엔진을 이용해서 사용하는 방식입니다. 즉 추론이란 이미 만들어서 배포한 모델을 활용해 사용자의 질문에 답하거나...","categories": ["Practical Lab","Generative AI"],
         "tags": [],
-        "url": "/llm/ollama/sllm/%EC%83%9D%EC%84%B1%ED%98%95-ai/ollala-101/",
+        "url": "/practical%20lab/generative%20ai/ollala-101/",
         "teaser": null
       },{
         "title": "AI를 이용한 바이브 스터디",
-        "excerpt":"바이브 스터디를 해보면 어떨까 ? 1999년, 제 개발 여정은 PHP로 시작해 C, C++, Python, Ruby, Javascript, Go 등 다양한 언어를 거쳐왔습니다. 다채로운 언어만큼이나 경험한 분야도 넓습니다. 웹 프로그래밍부터 시스템/네트워크 프로그래밍, 검색엔진, 블록체인, 빅데이터, 데이터베이스, 클라우드, DevOps, 그리고 최근에는 생성형 AI까지, 여러 영역을 넘나들며 개발해 왔습니다. AI 분야에서 일하다 보니 자연스럽게...","categories": ["LLM","생성형-AI","바이브코딩"],
+        "excerpt":"바이브 스터디를 해보면 어떨까 ? 1999년, 제 개발 여정은 PHP로 시작해 C, C++, Python, Ruby, Javascript, Go 등 다양한 언어를 거쳐왔습니다. 다채로운 언어만큼이나 경험한 분야도 넓습니다. 웹 프로그래밍부터 시스템/네트워크 프로그래밍, 검색엔진, 블록체인, 빅데이터, 데이터베이스, 클라우드, DevOps, 그리고 최근에는 생성형 AI까지, 여러 영역을 넘나들며 개발해 왔습니다. AI 분야에서 일하다 보니 자연스럽게...","categories": ["AI Engineering"],
         "tags": [],
-        "url": "/llm/%EC%83%9D%EC%84%B1%ED%98%95-ai/%EB%B0%94%EC%9D%B4%EB%B8%8C%EC%BD%94%EB%94%A9/study-ml-with-llm/",
+        "url": "/ai%20engineering/study-ml-with-llm/",
         "teaser": null
       },{
         "title": "Gemini의 멀티모달 기능 활용",
-        "excerpt":"개요 Gemini를 활용한 다양한 멀티모달 사용 사례를 탐색. 구글 클라우드 교육에 사용되는 노트북 코드를 기반으로 작성. 사용 LLM: Gemini Flash Gemini Flash는 Pro에 비해 비용과 시간이 최적화되어 있는 모델. 모든 모달리티에 대해 최대 100만 개의 토큰 컨텍스트 창을 가짐. 목표 Python용 Gen AI SDK를 사용하여 아래의 작업을 순차적으로 수행. 텍스트...","categories": ["LLM","생성형-AI","Quick","Lab"],
+        "excerpt":"개요 Gemini를 활용한 다양한 멀티모달 사용 사례를 탐색. 구글 클라우드 교육에 사용되는 노트북 코드를 기반으로 작성. 사용 LLM: Gemini Flash Gemini Flash는 Pro에 비해 비용과 시간이 최적화되어 있는 모델. 모든 모달리티에 대해 최대 100만 개의 토큰 컨텍스트 창을 가짐. 목표 Python용 Gen AI SDK를 사용하여 아래의 작업을 순차적으로 수행. 텍스트...","categories": ["Generative AI","Practical Lab"],
         "tags": [],
-        "url": "/llm/%EC%83%9D%EC%84%B1%ED%98%95-ai/quick/lab/multimodal/",
+        "url": "/generative%20ai/practical%20lab/multimodal/",
         "teaser": null
       },{
         "title": "Imagen 4 모델을 활용한 이미지 생성",
-        "excerpt":"개요 Imagen 4 를 활용한 이미지 생성 사례를 탐색. 구글 클라우드 교육에 사용되는 노트북 코드를 기반으로 작성. 사용 LLM: Imagen 4, Imagen 4 Fast 및 Imagen 4 Ultra 모델 목표 Python용 Gen AI SDK를 사용하여 아래의 방식을 테스트. 스타일 다양성 다국어 프롬프트 지원 및 향상 고품질 사실적 표현 텍스트 렌더링...","categories": ["LLM","생성형-AI","Quick","Lab"],
+        "excerpt":"개요 Imagen 4 를 활용한 이미지 생성 사례를 탐색. 구글 클라우드 교육에 사용되는 노트북 코드를 기반으로 작성. 사용 LLM: Imagen 4, Imagen 4 Fast 및 Imagen 4 Ultra 모델 목표 Python용 Gen AI SDK를 사용하여 아래의 방식을 테스트. 스타일 다양성 다국어 프롬프트 지원 및 향상 고품질 사실적 표현 텍스트 렌더링...","categories": ["Generative AI","Practical Lab"],
         "tags": [],
-        "url": "/llm/%EC%83%9D%EC%84%B1%ED%98%95-ai/quick/lab/image_generation/",
+        "url": "/generative%20ai/practical%20lab/image_generation/",
         "teaser": null
       },{
         "title": "2026 2월 AI 트렌드 리포트: '에이전틱 분석'의 표준화와 버티컬 AX 전략",
-        "excerpt":"📝 본 포스팅은 최근 급격히 변화하고 있는 생성형 AI 시장의 기술 동향을 분석하고, 엔터프라이즈 환경에서 실질적인 비즈니스 가치를 창출하기 위한 전략적 로드맵을 공유하기 위해 작성되었습니다. 1. 주목해야 할 기술: ‘에이전틱 분석’의 표준화와 인간의 개입(Human-in-the-loop) 2026년 AI 기술의 패러다임은 ‘완전 자율’에 대한 환상을 넘어, 인간과 AI가 복잡한 워크플로우를 어떻게 최적으로 분담할...","categories": ["AI Trends","Tech Strategy"],
+        "excerpt":"📝 본 포스팅은 최근 급격히 변화하고 있는 생성형 AI 시장의 기술 동향을 분석하고, 엔터프라이즈 환경에서 실질적인 비즈니스 가치를 창출하기 위한 전략적 로드맵을 공유하기 위해 작성되었습니다. 1. 주목해야 할 기술: ‘에이전틱 분석’의 표준화와 인간의 개입(Human-in-the-loop) 2026년 AI 기술의 패러다임은 ‘완전 자율’에 대한 환상을 넘어, 인간과 AI가 복잡한 워크플로우를 어떻게 최적으로 분담할...","categories": ["AI Intelligence","Tech Strategy"],
         "tags": ["AgenticAnalytics","BigTech","HybridGrounding","VerticalAX","NL2SQL"],
-        "url": "/ai%20trends/tech%20strategy/agentgo-market-strategy-intelligence-vol01/",
+        "url": "/ai%20intelligence/tech%20strategy/agentgo-market-strategy-intelligence-vol01/",
         "teaser": null
       }]
